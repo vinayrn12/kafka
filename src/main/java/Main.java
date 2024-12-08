a9084749ece430092f41df033c0adb72f1aebc13
vinay <vinayvrn12@gmail.com>
2024-12-08 21:00:03 +0530

    Setting up server socket on port 9092

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,28 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+     ServerSocket serverSocket;
+     Socket clientSocket = null;
+     int port = 9092;
+     try {
+       serverSocket = new ServerSocket(port);
 
-    // Uncomment this block to pass the first stage
-    // 
-    // ServerSocket serverSocket = null;
-    // Socket clientSocket = null;
-    // int port = 9092;
-    // try {
-    //   serverSocket = new ServerSocket(port);
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //   // Wait for connection from client.
-    //   clientSocket = serverSocket.accept();
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // } finally {
-    //   try {
-    //     if (clientSocket != null) {
-    //       clientSocket.close();
-    //     }
-    //   } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    //   }
-    // }
+       // Since the tester restarts your program quite often, setting SO_REUSEADDR
+       // ensures that we don't run into 'Address already in use' errors
+       serverSocket.setReuseAddress(true);
+
+       // Wait for connection from client.
+       clientSocket = serverSocket.accept();
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     } finally {
+       try {
+         if (clientSocket != null) {
+           clientSocket.close();
+         }
+       } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+       }
+     }
   }
 }
